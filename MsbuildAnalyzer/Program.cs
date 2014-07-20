eea74e905575deaa2dd637da1289f3e127090dcc
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-07-20 12:51:47 -0700

    adding sample method showing how to build with custom logger. needs refactoring

--- MsbuildAnalyzer/Program.cs
+++ MsbuildAnalyzer/Program.cs
@@ -4,9 +4,14 @@
     using System.Linq;
     using System.Text;
     using MsbuildAnalyzer.Common;
+    using Microsoft.Build.Evaluation;
+    using Microsoft.Build.Execution;
+    using MsbuildAnalyzer.Common.Loggers;
+    using Microsoft.Build.Framework;
 
     public class Program {
         static void Main(string[] args) {
+
             if (args != null && args.Length >= 1) {
                 CommandLineOptionsBuilder builder = new CommandLineOptionsBuilder();
                 CommandLineOptions options = builder.GetOptions(args);
@@ -27,6 +32,29 @@
             }
 
 
+        }
+
+        // this method is not being called yet, this is just prototype. i'll refactor
+        private static void BuildAndAnalyze() {
+            // https://github.com/bdachev/CSTests/blob/4a3056f2a821fc47362ef33eab0d22249776f984/MSBuildTest/Program.cs#L37
+            string projToBuild = @"C:\temp\msbuild\proj1.proj";
+
+            var pc = new ProjectCollection();
+            var diagLogger = new DiagnosticXmlLogger();
+            pc.RegisterLogger(diagLogger);
+
+            var proj = pc.LoadProject(projToBuild);
+            var projInst = proj.CreateProjectInstance();
+            var buildManager = new BuildManager();
+            var buildParams = new BuildParameters();
+            buildParams.Loggers = new ILogger[] { diagLogger };
+            buildManager.BeginBuild(buildParams);
+
+            var brd = new BuildRequestData(projInst, new string[] { "Demo" }, null, BuildRequestDataFlags.ReplaceExistingProjectInstance);
+            var submission = buildManager.PendBuildRequest(brd);
+            var buildResult = submission.Execute();
+
+            buildManager.EndBuild();
         }
 
         static void PrintUsage() {
